50195c17aa1fc0ee34c2a2bdc9f1ec84ca133144
Titanmito <chretien.clement@yahoo.fr>
2020-10-27 21:07:06 +0100

    perso bouge et decors interactif

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public float speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            movement += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            movement += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            movement += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            movement += Vector3.down;
+        }
+
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
